b95aa8322eb7db0687b39d2bdccfcf564a47fe5e
Johnny Karam <JohnnyKaram@protonmail.com>
2021-12-07 23:35:12 +0200

    first commit

---
+++ backend/functions/index.js
@@ -0,0 +1,11 @@
+const functions = require("firebase-functions");
+
+const admin = require("firebase-admin");
+admin.initializeApp();
+const db = admin.firestore();
+exports.newUser = functions.auth.user().onCreate((user) => {
+  return db
+      .collection("user")
+      .doc(user.uid)
+      .create(JSON.parse(JSON.stringify(user)));
+});
